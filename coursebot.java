58d9ecf80af72ed9334b5f6b762296d8a8c74850
jmachado11 <jm811machado@gmail.com>
2024-03-23 15:16:03 -0400

    summary

--- coursebot.java
+++ coursebot.java
@@ -1,1 +1,15 @@
 //Contains Code for the AI CourseBot
+<<<<<<< Updated upstream
+=======
+<<<<<<< HEAD
+import java.io.*;
+import java.util.*;
+
+public class coursebot{
+    public coursebot(){
+        
+    }
+}
+=======
+>>>>>>> deb5a7a5bbdd5c8d506b2de6908dea11a89bef6c
+>>>>>>> Stashed changes
